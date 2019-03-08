50020e94007714d31c132560b8cdb1e4700b5a4a
Katharine Hyatt <khyatt@flatironinstitute.org>
2019-03-08 08:26:24 -0500

    Doc free_memory and total_memory

--- base/sysinfo.jl
+++ base/sysinfo.jl
@@ -234,7 +234,18 @@ function loadavg()
     return loadavg_
 end
 
+"""
+    Sys.free_memory()
+
+Get the total free memory in RAM in kilobytes.
+"""
 free_memory() = ccall(:uv_get_free_memory, UInt64, ())
+
+"""
+    Sys.total_memory()
+
+Get the total memory in RAM (used and unused) in kilobytes.
+"""
 total_memory() = ccall(:uv_get_total_memory, UInt64, ())
 
 """
